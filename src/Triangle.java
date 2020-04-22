914bc00f5de044af9312d443be1d066405f349b1
OGO.KEI <ogoke9011@gmail.com>
2020-04-22 18:30:45 +0900

    課題：FizzBuzz問題

--- src/Triangle.java
+++ src/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle {
         int a = 8;
         int b = 4;
         int c = 5;
-        int aaa;
+
         if(a == b && b == c && c == a) {
             System.out.println("この三角形は正三角形です。");
         } else if(a == b || b == c || c == a) {
