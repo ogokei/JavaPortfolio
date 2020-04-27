5f6488e9148eefe4895088611b41bbb1e43fa373
OGO.KEI <ogoke9011@gmail.com>
2020-04-27 14:40:04 +0900

    lesson6修正

--- src/kozeni.java
+++ src/kozeni.java
@@ -4,16 +4,15 @@ public class kozeni {
 
     public static void main(String[] args) {
 
-        int money = 0;
         int[]  num = {1,0,2,2,8,3
                 };
 
-        money = num[0];
-        money = money + num[1] * 5;
-        money = money + num[2] * 10;
-        money = money + num[3] * 50;
-        money = money + num[4] * 100;
-        money = money + num[5] * 500;
+    int money  = num[0];
+        money += num[1] * 5;
+        money += num[2] * 10;
+        money += num[3] * 50;
+        money += num[4] * 100;
+        money += num[5] * 500;
 
         System.out.println("小銭は" + money + "円あります。");
     }
